cd717b17922277cd2d53cf994624ca8097a7ac2a
wonowonow <123870616+wonowonow@users.noreply.github.com>
2023-11-30 21:12:11 +0900

    test : 카드 전체 목록 불러오기 테스트

--- src/test/java/com/sparta/todoapp/domain/card/service/CardServiceTest.java
+++ src/test/java/com/sparta/todoapp/domain/card/service/CardServiceTest.java
@@ -6,11 +6,16 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import com.sparta.todoapp.domain.card.dto.CardListResponseDto;
 import com.sparta.todoapp.domain.card.dto.CardPostRequestDto;
 import com.sparta.todoapp.domain.card.dto.CardResponseDto;
 import com.sparta.todoapp.domain.card.entity.Card;
 import com.sparta.todoapp.domain.card.repository.CardRepository;
 import com.sparta.todoapp.domain.user.entity.User;
+import com.sparta.todoapp.domain.user.entity.UserRoleEnum;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -62,5 +67,31 @@ class CardServiceTest {
         Assertions.assertEquals(card.getTitle(), result.getTitle());
         Assertions.assertEquals(card.getContent(), result.getContent());
         Assertions.assertEquals(card.getUser().getUsername(), result.getAuthor());
+    }
+
+    @Test
+    @DisplayName("카드 여러개 불러오기 테스트")
+    void test3() {
+        // given
+        CardService cardService = new CardService(cardRepository);
+        User user1 = new User("username1","password1", UserRoleEnum.USER);
+        User user2 = new User("username2", "password2", UserRoleEnum.USER);
+        List<Card> cardList = new ArrayList<>();
+        cardList.add(new Card("제목 1", "내용 1", user1));
+        cardList.add(new Card("제목 2", "내용 2", user2));
+        cardList.add(new Card("제목 3", "내용 3", user1));
+        cardList.add(new Card("제목 4", "내용 4", user1));
+        cardList.add(new Card("제목 5", "내용 5", user1));
+        cardList.add(new Card("제목 6", "내용 6", user1));
+        cardList.add(new Card("제목 7", "내용 7", user1));
+        given(cardRepository.findAllByOrderByCreatedAtDesc()).willReturn(cardList);
+        // when
+        Map<String, List<CardListResponseDto>> result = cardService.getTodoCards();
+        // then
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(2, result.size());
+        Assertions.assertEquals(1, result.get(user2.getUsername()).size());
+        Assertions.assertEquals(6, result.get(user1.getUsername()).size());
+        Assertions.assertEquals("제목 3", result.get(user1.getUsername()).get(1).getTitle());
     }
 }
