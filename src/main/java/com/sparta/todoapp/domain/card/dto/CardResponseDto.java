c019e514c8f08cb55a22c8dedd2b287f3923bfba
wonowonow <123870616+wonowonow@users.noreply.github.com>
2023-12-23 23:40:58 +0900

    refactor: CardResponseDto 댓글 반환 기능 추가

--- src/main/java/com/sparta/todoapp/domain/card/dto/CardResponseDto.java
+++ src/main/java/com/sparta/todoapp/domain/card/dto/CardResponseDto.java
@@ -1,7 +1,10 @@
 package com.sparta.todoapp.domain.card.dto;
 
 import com.sparta.todoapp.domain.card.entity.Card;
+import com.sparta.todoapp.domain.comment.dto.CommentResponseDto;
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,6 +19,7 @@ public class CardResponseDto {
     private String author;
     private Boolean isDone;
     private LocalDateTime createdAt;
+    private List<CommentResponseDto> comments;
 
     public CardResponseDto(Card card) {
         this.title = card.getTitle();
@@ -24,6 +28,10 @@ public class CardResponseDto {
         this.author = card.getUser().getUsername();
         this.isDone = card.getIsDone();
         this.createdAt = card.getCreatedAt();
+        this.comments = card.getComments()
+                            .stream()
+                            .map(CommentResponseDto::new)
+                            .collect(Collectors.toList());
     }
 
     public CardResponseDto(String title, String content, String imageUrl, String author,
